82fbc20b964977a6cab3e72bdf97dcafc6de2ee9
Panpan Han <hanpanpan200@gmail.com>
2018-01-17 09:42:00 +0800

    feat: resolve mongoose connect  warnning & add unified response for 404

--- server.js
+++ server.js
@@ -7,22 +7,18 @@ var express = require('express'),
   
 // mongoose instance connection url connection
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/Tododb'); 
-
+mongoose.connect('mongodb://localhost/Tododb', { useMongoClient: true }); 
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use(function(req, res) {
-  res.status(404).send({ url: req.originalUrl + ' not found.' })
-})
-
-
 var routes = require('./api/routes/todoListRoutes'); //importing route
 routes(app); //register the route
 
-
 app.listen(port);
 
+app.use(function(req, res) {
+  res.status(404).send({ url: req.originalUrl + ' not found.' })
+})
 
 console.log('todo list RESTful API server started on: ' + port);
